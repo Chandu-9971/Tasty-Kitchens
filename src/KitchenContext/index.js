bf90352577e77f652f2fa59912c42dab4d11eeaf
Chandu Munagapati <chandhu.munagapati@gmail.com>
2023-01-09 16:56:20 +0000

    tasty kitchens

---
+++ src/KitchenContext/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const KitchenContext = React.createContext({
+  addBtn: false,
+  filter: () => {},
+  onAddBtn: () => {},
+  sortByOptions: [],
+  temporaryData: [],
+  removeItem: () => {},
+  updateLocalData: () => {},
+  checkZero: () => {},
+  stateEmpty: () => {},
+})
+export default KitchenContext
